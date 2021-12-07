87c6c372e3f7a16fa7662b3ee9c1fd42d0d006bd
Vladyslav Kekukh <vladyslavkekukh@iMac-Vladyslav.local>
2021-12-07 21:33:15 +0100

    init

---
+++ db-regio/src/main/java/dev/vkekukh/trainsimulator/dbregio/DbRegioApplication.java
@@ -0,0 +1,13 @@
+package dev.vkekukh.trainsimulator.dbregio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRegioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbRegioApplication.class, args);
+    }
+
+}
---
+++ db-regio/src/test/java/dev/vkekukh/trainsimulator/dbregio/DbRegioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vkekukh.trainsimulator.dbregio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbRegioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
